2d864e453b20be20c60b173728d5e8b4e1922cb4
Mohd Saif <iamg6102@gmail.com>
2025-08-27 15:49:34 +0530

    app confiq

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 import connectDB from './db/index.js';
+import app from './app.js';
 
-connectDB();
+connectDB().then( () => {
+    app.listen(process.env.PORT || 5100, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch(err => console.log(err));
