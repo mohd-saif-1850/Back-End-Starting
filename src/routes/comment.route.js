358a726c17e00b6f0c693a42f5ea2f7bfd68e1b7
Mohd Saif <iamg6102@gmail.com>
2025-09-08 21:47:19 +0530

    feat: add userComments route to fetch comments by user

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.controller.js"
-import { createComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
+import { createComment, deleteComment, updateComment, userComments } from "../controllers/comment.controller.js";
 
 const router = Router()
 
     router.route("/create-comment").post(verifyJWT,createComment)
     router.route("/update-comment").patch(verifyJWT,updateComment)
     router.route("/delete-comment").delete(verifyJWT,deleteComment)
+    router.route("/user-comments").get(verifyJWT,userComments)
 
 export default router;
