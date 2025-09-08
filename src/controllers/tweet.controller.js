e4398957889d636eaf67c4dac3451498025baed9
Mohd Saif <iamg6102@gmail.com>
2025-09-08 20:38:51 +0530

    feat: add deleteTweet functionality to handle tweet deletions

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -46,4 +46,20 @@ const updateTweet = asyncHandler( async(req,res) => {
     return res.status(200).json(new apiResponse(200,newTweet,"Tweet Updated Successfully !"))
 })
 
-export {createTweet,updateTweet}
+const deleteTweet = asyncHandler( async(req,res) => {
+    const {tweetId} = req.body
+
+    if (!tweetId) {
+        throw new apiError(402,"Tweet ID is Required !")
+    }
+
+    const tweetDelete = await Tweet.findByIdAndDelete(tweetId)
+
+    if (!tweetDelete) {
+        throw new apiError(500,"Server Failed to Delete a Tweet !")
+    }
+
+    return res.status(200).json(new apiResponse(200,tweetDelete,"Tweet Deleted Successfully !"))
+})
+
+export {createTweet,updateTweet,deleteTweet}
