57d74d680330e137312abdd5d888742ee5be1299
Mohd Saif <iamg6102@gmail.com>
2025-09-08 20:47:37 +0530

    feat: add getUserTweets route to fetch user's tweets

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.controller.js"
-import { createTweet, deleteTweet, updateTweet } from "../controllers/tweet.controller.js";
+import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
     router.route("/create-tweet").post(verifyJWT,createTweet)
     router.route("/update-tweet").patch(verifyJWT,updateTweet)
     router.route("/delete-tweet").delete(verifyJWT,deleteTweet)
-
+    router.route("/get-tweets").get(verifyJWT,getUserTweets)
 
 export default router;
