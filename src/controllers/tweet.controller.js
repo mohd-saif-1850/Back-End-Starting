c2121442ad8f6be3a69f7b8da3feb0439053c042
Mohd Saif <iamg6102@gmail.com>
2025-09-08 20:25:32 +0530

    feat: add updateTweet functionality to handle tweet updates

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -24,4 +24,26 @@ const createTweet = asyncHandler( async(req,res) => {
     return res.status(200).json(new apiResponse(200,tweetCreation,"Tweet Created Successfully !"))
 })
 
-export {createTweet}
+const updateTweet = asyncHandler( async(req,res) => {
+    const {tweetId, content} = req.body
+
+    if (!tweetId) {
+        throw new apiError(402,"Tweet ID is Required !")
+    }
+
+    if (!content) {
+        throw new apiError(402,"Content is Required !")
+    }
+
+    const newTweet = await Tweet.findById(tweetId,{
+        $set : {content}
+    },{new : true})
+
+    if (!newTweet) {
+        throw new apiError(500,"Server Failed to Update Tweet !")
+    }
+
+    return res.status(200).json(new apiResponse(200,newTweet,"Tweet Updated Successfully !"))
+})
+
+export {createTweet,updateTweet}
