ad99b13833b66fd9bf476aafa6fa5a2d8edabc01
Mohd Saif <iamg6102@gmail.com>
2025-09-08 21:32:18 +0530

    feat: add updateComment route to enable comment updates

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.controller.js"
-import { createComment } from "../controllers/comment.controller.js";
+import { createComment, updateComment } from "../controllers/comment.controller.js";
 
 const router = Router()
 
     router.route("/create-comment").post(verifyJWT,createComment)
+    router.route("/update-comment").patch(verifyJWT,updateComment)
 
 
 export default router;
