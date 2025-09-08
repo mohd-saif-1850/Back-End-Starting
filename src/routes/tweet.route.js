f4b38f399154422386fbc5bcbbf57b4334958037
Mohd Saif <iamg6102@gmail.com>
2025-09-08 20:27:13 +0530

    feat: import updateTweet in tweet route for handling tweet updates

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.controller.js"
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
     router.route("/create-tweet").post(verifyJWT,createTweet)
-
+    router.route("/update-tweet").patch(verifyJWT,updateTweet)
 
 
 export default router;
