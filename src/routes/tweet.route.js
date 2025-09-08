e376906b64a69c5ab100a56c79db981a7d79f270
Mohd Saif <iamg6102@gmail.com>
2025-09-08 20:40:35 +0530

    feat: import deleteTweet in tweet route for handling tweet deletions

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.controller.js"
-import { createTweet, updateTweet } from "../controllers/tweet.controller.js";
+import { createTweet, deleteTweet, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
     router.route("/create-tweet").post(verifyJWT,createTweet)
     router.route("/update-tweet").patch(verifyJWT,updateTweet)
+    router.route("/delete-tweet").delete(verifyJWT,deleteTweet)
 
 
 export default router;
