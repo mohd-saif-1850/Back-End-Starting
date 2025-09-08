f47b10fef1aaf3f758fd998ca7dd52ffacd8ae30
Mohd Saif <iamg6102@gmail.com>
2025-09-08 16:44:25 +0530

    createTweet in tweet route

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,18 +1,10 @@
 import { Router } from "express";
-import {upload} from "../middlewares/multer.middleware.js"
 import verifyJWT from "../middlewares/auth.controller.js"
+import { createTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
-
-
-
-
-
-
-
-
-
+    router.route("/create-tweet").post(verifyJWT,createTweet)
 
 
 
